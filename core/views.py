088513d33805542e83ddeede89c2f3396584374c
graple254 <graple254@gmail.com>
2026-04-24 02:23:54 +0300

    added startup instructions

--- core/views.py
+++ core/views.py
@@ -11,3 +11,8 @@ def home_view(request):
 @login_required
 def connect_view(request):
     return render(request, 'files/connect.html')
+
+
+##StartUp commands;  daphne -b 0.0.0.0 -p 8000 chichi.asgi:application
+## Redis Startup command; redis-server --port 6380
+## Remember to change csrf origins in settings.py and google oauth credentials.
